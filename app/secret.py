87479c37105507ff003e3f0572b33fc4bf1acae0
Qu1s <trof1m00@mail.ru>
2020-09-22 23:25:55 +0300

    Changed secret.py

--- app/secret.py
+++ app/secret.py
@@ -1,3 +1,5 @@
 app_secret_key = ${{secrets.APP_KEY}}
 DB_link = ${{secrets.DB_LINK}}
-DB_name = ${{secrets.DB_LINK}} 
+DB_name = ${{secrets.DB_name}} 
+DB_collection_users = ${{secrets.DB_collection_users}}
+DB_collection_works = ${{secrets.DB_collection_works}} 
