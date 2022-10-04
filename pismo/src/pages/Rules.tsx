bc4477bd6df36fe8ec80435323804076ec32c872
BSOD_levch <levch98@mail.ru>
2022-10-04 22:06:20 +0300

    fix: первичное оформление Правил, Загрузки и Работы

--- pismo/src/pages/Rules.tsx
+++ pismo/src/pages/Rules.tsx
@@ -1,3 +1,40 @@
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import MainBar from '../components/MainBar'
+
 function Rules() {
+    return (
+        <>
+        <MainBar />
+        <h1>Правила конкурса</h1>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Paper elevation={3} >
+                    Правило 1
+                </Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper elevation={3} >
+                    Правило 2
+                </Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper elevation={3} >
+                    Правило 3
+                </Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper elevation={3} >
+                    Правило 4
+                </Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper elevation={3} >
+                    Правило 5
+                </Paper>
+            </Grid>
+        </Grid>
+        </>
+    )
 }
 export default Rules
--- pismo/src/pages/Submit.tsx
+++ pismo/src/pages/Submit.tsx
@@ -1,3 +1,20 @@
+import TextareaAutosize from '@mui/material/TextareaAutosize'
+import Button from '@mui/material/Button'
+import MainBar from '../components/MainBar'
+
 function Submit() {
+    return (
+        <>
+        <MainBar />
+        <h1>Загрузить работу</h1>
+
+        <p>Напишите или вставьте текст Вашей работы</p>
+
+        <TextareaAutosize aria-label="empty textarea" style={{ width: 800, height: 800 }}/>
+        <br/>
+        <br/>
+        <Button variant="contained">Загрузить</Button>
+        </>
+    )
 }
 export default Submit
